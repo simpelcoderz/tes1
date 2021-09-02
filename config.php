bb5ff69e06b7e1528b11e84c500c57d78b3b59d3
Hemッ <76958699+simpelcoderz@users.noreply.github.com>
2021-09-02 09:28:08 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db_handle = pg_connect("host=localhost dbname=edb user=enterprisedb password=postgres");
+$db_handle = pg_connect("host=ec2-18-211-41-246.compute-1.amazonaws.com dbname=d9u6bsoigamjmu user=teuqsaumeqnebe password=e57afbe83eb4e8780a2298194244ae797f4be8b08b2a20df238a77d0d7cbf0b5");
 if ($db_handle) {
 echo 'Connection attempt succeeded.';
 } else {
